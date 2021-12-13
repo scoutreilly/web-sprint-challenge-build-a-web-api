15c3db8582a7fb035a462eb75a559374c3f2adc4
scoutreilly <scouty33@gmail.com>
2021-12-12 20:44:13 -0700

    middleware finished

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,36 @@
 // add middlewares here related to actions
+const actionsModel = require("./actions-model");
+
+async function validateActionId(req, res, next) {
+  try {
+    const action = await actionsModel.get(req.params.id);
+    if (!action) {
+      res
+        .status(404)
+        .json({ message: "action with give id could not be found" });
+    } else {
+      req.action = action;
+      next();
+    }
+  } catch {
+    res.status(500).json({ message: "action could not be validated" });
+  }
+}
+
+async function validateAction(req, res, next) {
+  const { project_id, description, notes, completed } = req.body;
+  if (!project_id) {
+    res.status(400).json({ message: "action with give id could not be found" });
+  }
+  if (!notes || !notes.trim) {
+    res.status(400).json({ message: "please provide action notes" });
+  } else {
+    req.project_id = project_id;
+    req.description = description.trim();
+    req.notes = notes.trim();
+    req.completed = completed;
+    next();
+  }
+}
+
+module.exports = { validateActionId, validateAction };
--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,36 @@
 // add middlewares here related to projects
+const projects = require("./projects-model");
+
+async function validateProjectId(req, res, next) {
+  try {
+    const { id } = req.params;
+    const project = await projects.get(id);
+    if (project) {
+      req.params = project;
+      next();
+    } else {
+      next({
+        status: 404,
+        message: "project with given id could not be found",
+      });
+    }
+  } catch {
+    res.status(500).json({ message: "project could not be validated" });
+  }
+}
+
+async function validateProject(req, res, next) {
+  const { name, description, completed } = req.body;
+  if (!name || !name.trim()) {
+    res.status(400).json({ message: "please provide a name" });
+  } else if (!description || !description.trim()) {
+    res.status(400).json({ message: "please provide a description" });
+  } else {
+    req.name = name.trim();
+    req.description = description.trim();
+    req.completed = completed;
+    next();
+  }
+}
+
+module.exports = { validateProjectId, validateProject };
